854050091fa27b0a728a504a55780ad24839df37
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-07-07 17:35:01 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <section>
+      <h3>Arcado</h3>
+      <Button>pay now</Button>
+    </section>
+  )
+}
+
+export default App
